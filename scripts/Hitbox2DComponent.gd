85dfe407e4ff8dbfa9caa7ae7c63d9754836be20
Michael Muniz <mcmuniz218@gmail.com>
2024-10-05 15:09:59 -0400

    add on_hit signal

--- scripts/Hitbox2DComponent.gd
+++ scripts/Hitbox2DComponent.gd
@@ -1,7 +1,9 @@
 class_name Hitbox2DComponent extends Area2D
 
+signal on_hit(attack: Attack)
 @export var health_component: HealthComponent
 
 func damage(attack: Attack):
 	if health_component:
+		on_hit(attack)
 		health_component.damage(attack)
