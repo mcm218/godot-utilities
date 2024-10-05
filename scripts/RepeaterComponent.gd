e597d6faa80727ac2788fe216f5e8a05f0f64ea4
Michael Muniz <mcmuniz218@gmail.com>
2024-10-05 15:09:20 -0400

    finish repeater component

--- scripts/RepeaterComponent.gd
+++ scripts/RepeaterComponent.gd
@@ -2,3 +2,14 @@ class_name RepeaterComponent extends Node
 
 @export var seconds_per_trigger: float = 1.0
 signal on_trigger
+
+var counter: float = 0.0
+
+func _ready():
+    counter = 0.0
+
+func _process(delta):
+    counter += delta
+    if counter >= seconds_per_trigger:
+        counter -= seconds_per_trigger
+        on_trigger.emit()
