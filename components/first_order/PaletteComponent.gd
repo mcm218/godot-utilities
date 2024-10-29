8edf452ed336558fd44e761599aefd599d5adfca
Michael Muniz <mcmuniz218@gmail.com>
2024-10-28 22:02:38 -0400

    wip

--- components/first_order/PaletteComponent.gd
+++ components/first_order/PaletteComponent.gd
@@ -1,15 +1,17 @@
 class_name PaletteComponent extends CanvasLayer
 
-@export var texture: Texture2D = load("../../../textures/palette")
+@export var texture: Texture2D = load("res://Hollow 1x.png")
 @export var flip: bool = false
-var sprite: Sprite2D
+@onready var sprite: Sprite2D = $Sprite2D
 
 func _ready():
-    sprite = Sprite2D.new()
-    add_child(sprite)
-
-    var loaded_material: ShaderMaterial = load("../../shaders/apply_palette.tres")
-    if loaded_material is ShaderMaterial:
-        loaded_material.set_shader_param("palette", texture)
-        loaded_material.set_shader_param("flip", flip)
-        sprite.material = loaded_material
+	print_debug(texture)
+	print_debug(flip)
+	print_debug(sprite)
+	var shader: Shader = load("res://utilities/shaders/apply_palette.gdshader")
+	print_debug(shader)
+	var material: ShaderMaterial = ShaderMaterial.new()
+	material.shader = shader;
+	material.set_shader_parameter("palette", texture)
+	material.set_shader_parameter("flip", flip)
+	sprite.material = material
