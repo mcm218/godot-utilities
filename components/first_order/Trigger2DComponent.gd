1bc238d53d04f65f36eff2a960513f68901c99f7
Michael Muniz <mcmuniz218@gmail.com>
2024-11-12 18:59:52 -0500

    Fix signals

--- components/first_order/Trigger2DComponent.gd
+++ components/first_order/Trigger2DComponent.gd
@@ -2,5 +2,9 @@ class_name Trigger2DComponent extends Area2D
 
 signal on_trigger(body: Node)
 
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
 func _on_body_entered(body: Node) -> void:
-	emit_signal("triggered", body)
+	print_debug("TRIGGERED BY " + body.name);
+	emit_signal("on_trigger", body)
